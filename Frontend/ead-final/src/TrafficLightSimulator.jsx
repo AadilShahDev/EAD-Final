67e8f52de69d67572c6ec80892bb24417ce7b967
Aadil Bukhari <theaadilbukhari@gmail.com>
2025-06-03 11:46:37 +0500

    Feature:implemented light cycle with useEffects

--- Frontend/ead-final/src/TrafficLightSimulator.jsx
+++ Frontend/ead-final/src/TrafficLightSimulator.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function TrafficLightSimulator() {
+    let lights = ['red','green','yellow']
+    let i=-1
+    let [light,setLight] = useState(lights[0])
+    
+
+    useEffect(()=>{
+        setInterval(()=>{
+             i++
+             
+            setLight(lights[i])
+
+            if(i==2){
+                i=-1
+             }
+            
+        },3000)
+    })
   return (
-    <div>
-      TrafficLightSimulator
+    <div>Traffic Light Simulator
+    <div className='h-full flex justify-center items-center'>
+      <div className='bg-gray-400 flex flex-col w-64 h-[80%] justify-center items-center '>
+        <div className={light=='red' ? 'bg-red-500 border rounded-[50%] w-24 h-24 m-5' : 'bg-gray-400 border rounded-[50%] w-24 h-24 m-5'} ></div>
+        <div className={light=='yellow' ? 'bg-yellow-500 border rounded-[50%] w-24 h-24 m-5' : 'bg-gray-400 border rounded-[50%] w-24 h-24 m-5'}></div>
+        <div className={light=='green' ? 'bg-green-500 border rounded-[50%] w-24 h-24 m-5' : 'bg-gray-400 border rounded-[50%] w-24 h-24 m-5'}></div>
+      </div>
+    </div>
     </div>
   )
 }
